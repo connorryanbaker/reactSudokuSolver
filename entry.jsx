ab0b3e24f146219f059c7841c483f9374dd5f160
Connor Baker <connorbaker@gmail.com>
2019-04-14 18:09:22 -0400

    add rows, columns and squares methods to board class and switch state to hold values

--- entry.jsx
+++ entry.jsx
@@ -6,5 +6,5 @@ import Board from './src/board';
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
   const str = "040000179002008054006005008080070910050090030019060040300400700570100200928000060";
-  ReactDOM.render(<Board startString={str}/>, root);
+  ReactDOM.render(<Board startString={str} />, root);
 });
--- src/board.jsx
+++ src/board.jsx
@@ -7,6 +7,9 @@ class Board extends React.Component {
       grid: this.configureGrid(this.props.startString)
     };
     this.configureGrid = this.configureGrid.bind(this);
+    this.rows = this.rows.bind(this);
+    this.columns = this.columns.bind(this);
+    this.squares = this.squares.bind(this);
   }
 
   configureGrid(str) {
@@ -17,14 +20,63 @@ class Board extends React.Component {
     }
     return grid.map(row => {
       return row.map(el => {
-        return <Tile value={el} />
+        return parseInt(el);
       });
     });
   }
 
+  columns() {
+    const columns = Array.from(this.state.grid, () => new Array(9));
+    for (let i = 0; i < 9; i++) {
+      for (let j = 0; j < 9; j++) {
+        columns[j][i] = this.state.grid[i][j];
+      }
+    }
+    return columns;
+  }
+
+  rows() {
+    return this.state.grid;
+  }
+
+  squares() {
+    const squares = Array.from(this.state.grid, () => []);
+    for (let i = 0; i < 9; i++) {
+      for (let j = 0; j < 9; j++) {
+        if (j < 3) {
+          if (i < 3) {
+            squares[0].push(this.state.grid[i][j]);
+          } else if (i < 6) {
+            squares[3].push(this.state.grid[i][j]);
+          } else {
+            squares[6].push(this.state.grid[i][j]);
+          };
+        } else if (j < 6) {
+          if (i < 3) {
+            squares[1].push(this.state.grid[i][j]);
+          } else if (i < 6) {
+            squares[4].push(this.state.grid[i][j]);
+          } else {
+            squares[7].push(this.state.grid[i][j]);
+          };
+        } else {
+          if (i < 3) {
+            squares[2].push(this.state.grid[i][j]);
+          } else if (i < 6) {
+            squares[5].push(this.state.grid[i][j]);
+          } else {
+            squares[8].push(this.state.grid[i][j]);
+          };
+        }
+      }
+    }
+    return squares;
+  }
+
   render() {
-    const uls = this.state.grid.map(row => {
-      return <ul className="grid-row">{row}</ul>;
+    console.log(this.columns());
+    const uls = this.state.grid.map((row, i) => {
+      return <ul className="grid-row">{row.map((el, idx) => <Tile value={el} key={idx * (i + 1)}/>)}</ul>;
     });
     return (
       <div>
